d551792c16e3b2c6156bb7ce029b3a946a7bddce
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-06-17 11:54:06 +0100

    lesson 16 refs

--- playlist/app.js
+++ playlist/app.js
@@ -1,41 +1,15 @@
-var data = {
-    name: 'Yoshi'
-}
-
-Vue.component('greeting', {
-    template: '<p>Hey there, i am {{ name }}. <button @click="changeName">Change Name</button></p>',
-    data: function(){
-        return data;
-    },
-    methods: {
-        changeName: function() {
-            this.name = 'Mario';
-        }
-    },
-});
-
 new Vue({
-    el: '#vue-app-one',
+    el: '#vue-app',
     data: {
-    
+        output: 'Your Fav Food'
     },
     methods: {
-    
+        readRefs: function() {
+            console.log(this.$refs.test.innerText);
+            this.output = this.$refs.input.value;
+        }
     },
     computed: {
       
-    },
-});
-
-new Vue({
-    el: '#vue-app-two',
-    data: { 
-  
-    },
-    methods: {
-    
-    },
-    computed: {
-    
     },
 });
