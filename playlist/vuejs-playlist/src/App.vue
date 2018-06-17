426540d376d434ebeb23010e12b1da54889259f4
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-06-17 12:49:30 +0100

    lesson 19 nesting components

--- playlist/vuejs-playlist/src/App.vue
+++ playlist/vuejs-playlist/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
+
 export default {
+  components: {
+    'ninjas': Ninjas,
+  },
   data () {
     return {
-      title: 'Your first vue file!'
-    }
-  },
-  methods: {
-    greeting: function() {
-      return 'Hello world.'
+      title: 'Ninja App'
     }
   }
 }
