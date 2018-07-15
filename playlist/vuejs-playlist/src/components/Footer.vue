5f9df5a93204d7b8be6ff68a7bbfe2246eb32534
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-07-15 12:03:10 +0100

    lesson 25 the event bus

--- playlist/vuejs-playlist/src/components/Footer.vue
+++ playlist/vuejs-playlist/src/components/Footer.vue
@@ -1,34 +1,37 @@
+
 <template>
     <footer>
-        <p>{{ copyright }} {{ title }}</p>
+        <p>Copyright 2017 {{ title }}</p>
     </footer>
 </template>
-
 <script>
-
+// imports
+import { bus } from '../main';
 export default {
     props: {
-        title: {
-            type: String
+      title: {
+        type: String,
+        required: true
+      }
+    },
+    data(){
+        return{
         }
     },
-    
-    data () {
-        return {
-        copyright: 'Copyright 2018'
-        }
+    created(){
+        bus.$on('titleChanged', (data) => {
+            this.title = data;
+        });
     }
 }
 </script>
-
 <style scoped>
-    footer {
-        background: #222;
-        padding: 6px;
-    }
-
-    p {
+footer{
+    background: #222;
+    padding: 6px;
+}
+p{
         color: lightgreen;
         text-align: center;
-    }
+}
 </style>
--- playlist/vuejs-playlist/src/components/Ninjas.vue
+++ playlist/vuejs-playlist/src/components/Ninjas.vue
@@ -1,62 +1,53 @@
 <template>
     <div id="ninjas">
         <ul>
-            <li 
-                v-for="ninja in ninjas"
-                @click="ninja.show = !ninja.show"
-            >
-            <h2>{{ ninja.name }}</h2>
-            <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
+            <li v-for="ninja in ninjas" v-on:click="ninja.show = !ninja.show">
+                <h2>{{ ninja.name }}</h2>
+                <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
             </li>
         </ul>
-        <button @click="deleteNinja">Delete Ninja</button>
+        <button v-on:click="deleteNinja">Delete a Ninja</button>
     </div>
 </template>
-
 <script>
-
 export default {
     props: {
-        ninjas: {
-            type: Array,
-            required: true
-        }
+      ninjas: {
+        type: Array,
+        required: true
+      }
     },
-    data () {
-        return {
-            
+    data(){
+        return{
         }
     },
     methods: {
-        deleteNinja: function() {
-            this.ninjas.pop();
-        }
-    },
+      deleteNinja: function(){
+        this.ninjas.pop();
+      }
+    }
 }
 </script>
-
 <style scoped>
-    #ninjas {
-        width: 100%;
-        max-width: 1200px;
-        margin: 40px auto;
-        padding: 0 20px;
-        box-sizing: border-box;
-    }
-
-    ul {
-        display: flex;
-        flex-wrap: wrap;
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        flex-grow: 1;
-        flex-basis: 300px;
-        text-align: center;
-        padding: 30px;
-        border: 1px solid #222;
-        margin: 10px;
-    }
+#ninjas{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+}
 </style>
