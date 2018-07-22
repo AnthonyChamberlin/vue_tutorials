c294a30b40b39db2a57e5b7042385925786bb20e
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-07-22 15:27:57 +0100

    lesson 43 posting to firebase

--- playlist/vuejs-playlist/src/components/add-blog.vue
+++ playlist/vuejs-playlist/src/components/add-blog.vue
@@ -60,11 +60,7 @@ export default {
     },
     methods: {
         post: function(){
-            this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-                title: this.blog.title,
-                body: this.blog.content,
-                userId: 1
-            }).then(function(data){
+            this.$http.post('https://vue-blog-app-fba92.firebaseio.com/posts.json', this.blog).then(function(data){
                 console.log(data);
                 this.submitted = true;
             });
