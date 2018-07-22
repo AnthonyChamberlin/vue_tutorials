c3e2f04c1efb1f3266363300c31d44cb7a65bef5
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-07-22 09:54:08 +0100

    lesson 33 get requests

--- playlist/vuejs-playlist/src/App.vue
+++ playlist/vuejs-playlist/src/App.vue
@@ -1,15 +1,24 @@
 <template>
     <div>
-        <add-blog></add-blog>
+        <div class="container">
+            <div id="left-col">
+                <add-blog></add-blog>
+            </div>
+            <div id="right-col">
+                <show-blogs></show-blogs>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import addBlog from './components/add-blog.vue';
+import showBlogs from './components/show-blogs.vue';
 
 export default {
     components: {
         'add-blog': addBlog,
+        'show-blogs': showBlogs,
     },
 
     data () {
@@ -27,10 +36,28 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css?family=Lato:100,300,400,700');
 body {
-    margin: 0;
+    margin: 50px;
     font-family: 'Lato', sans-serif;
     font-weight: 100;
     font-size: 18px;
     color: #202020;
+}
+
+.container {
+    position: relative;
+    height: 100vh;
+    width: 100%;
+    display: inline-block;
+}
+
+#left-col {
+    position: absolute;
+    left: 0;
+    width: 50%;
+}
+#right-col {
+    position: absolute;
+    right: 0;
+    width: 50%;
 }
 </style>
