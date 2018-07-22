d07e52804b96157056ed11e0c02686cc577adb5e
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-07-22 13:11:28 +0100

    lesson 41 add router links

--- playlist/vuejs-playlist/src/App.vue
+++ playlist/vuejs-playlist/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <app-header></app-header>
         <router-view></router-view>
     </div>
 </template>
@@ -8,12 +9,14 @@
 import addBlog from './components/add-blog.vue';
 import showBlogs from './components/show-blogs.vue';
 import listBlogs from './components/list-blogs.vue';
+import header from './components/header.vue';
 
 export default {
     components: {
         'add-blog': addBlog,
         'show-blogs': showBlogs,
         'list-blogs': listBlogs,
+        'app-header': header,
     },
 
     data () {
@@ -31,7 +34,7 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css?family=Lato:100,300,400,700');
 body {
-    margin: 50px;
+    margin: 0;
     font-family: 'Lato', sans-serif;
     font-weight: 100;
     font-size: 18px;
---
+++ playlist/vuejs-playlist/src/components/header.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav>
+        <ul>
+            <li><router-link to="/" exact>My Blogs</router-link></li>
+            <li><router-link to="/add" exact>Create Blog</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<style>
+ul{
+    list-style-type: none;
+    text-align: center;
+    margin: 0;
+}
+li{
+    display: inline-block;
+    margin: 0 10px;
+}
+a{
+    color: #fff;
+    text-decoration: none;
+    padding: 6px 8px;
+    border-radius: 10px;
+}
+nav{
+    background: #444;
+    padding: 24px 0;
+    margin-bottom: 60px;
+    box-shadow: 0 1px 5px #282828;
+}
+.router-link-active{
+    background: #eee;
+    color: #444;
+    padding: 5px 25px;
+    margin: 100px;
+}
+</style>
+
+<script>
+export default {
+    
+}
+</script>
